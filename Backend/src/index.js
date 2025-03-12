9bf53bbf088a07a9f81f44aad00ab8d38f463adb
Vivek Kumar Verma <110935058+vivekverma942003@users.noreply.github.com>
2025-03-12 16:00:08 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -25,9 +25,9 @@ app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")))
     app.get("*", (req,res)=>{
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+        res.sendFile(path.join(__dirname, "../Frontend","dist","index.html"))
     })
 }
 
